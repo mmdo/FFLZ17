7605d076f5645541c8ed4c6fd9e7dce7eff44139
Matthias Müller <matthias.mueller@uni-wuppertal.de>
2013-02-09 11:39:41 +0100

    1st step

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html> 
+<html lang="en">  
+<head>  
+    <meta charset="iso-8859-1">  
+    <title>Feuerwehr Dortmund-Persebeck</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+	<link rel="stylesheet" href="css/unsemantic-grid-responsive.css" />
+
+	
+</head>  
+<body> 
