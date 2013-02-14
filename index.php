75908d833a0770184e1a077785a785a3f5cdf53f
Matthias Müller <matthias.mueller@uni-wuppertal.de>
2013-02-14 21:44:42 +0100

    grid layout embedded
    unsemantic grid in index embedded;
    functions.php added due to enable user-specific menus;
    fixed the main loop, showing only entries of category 1
    modified the index as homepage with different divs showing excerpts of
    different pages
    
    to do:
    more-links
    top menu arrangement

--- index.php
+++ index.php
@@ -1,3 +1,98 @@
-<?php get_header(); ?>
-
-<?php get_footer(); ?>
+<!DOCTYPE html> 
+<html lang="de-DE">  
+<head>  
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />	
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+	<link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" type="text/css"/>
+	
+	
+</head>  
+<body> 
+	<div id="wrapper">
+		<header role="banner" class="grid-container">
+			<div class="grid-100">
+				<?php get_header(); ?>
+				<nav>
+					
+					<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+					
+				</nav>
+			</div>
+		</header>
+		<div role="main" class="grid-container">
+			<div class="grid-70 mobile-grid-100">
+				<p><strong>Aktuelles:</strong></p>
+				<!-- The loop -->
+				<?php query_posts( 'cat=1' ); ?>
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<?php the_title( '<h3>' , '</h3>' ); ?>
+				<?php the_content( $more_link_text, $stripteaser ); ?>
+				<?php the_excerpt(); ?> 
+				<?php endwhile; else: ?>
+				<p><?php _e('Momentan sind keine Beitr&auml;ge vorhanden.'); ?></p>
+				<?php endif; ?>
+				<!-- The loop end -->
+			</div>
+			<div class="grid-30 mobile-grid-100">
+				<div class="grid-100 mobile-grid-50">
+					<p><strong>N&auml;chste Termine:</strong></p>Der sch&auml;bige Rest. 
+				</div>
+				<div class="grid-100 mobile-grid-50">
+					<p><strong>Letzte Eins&auml;tze:</strong></p>
+					<table>
+						<?php query_posts('cat=2'); /* assign page id SINGLE POST EXCERPT*/ ?><!-- 48 Massenanfall von Verletzten -->
+						<?php if(have_posts() ) : while ( have_posts() ) : the_post(); /* start the loop */ ?>
+						<?php the_content(); /* prints the content */ ?>
+						<?php endwhile; else: ?>
+						<p><?php _e('Keine Eins&auml:tze eingetragen.'); ?></p>
+				<?php endif; ?>
+					</table>
+					
+				</div>
+			</div>
+			<div class="clear"></div>
+			<div class="grid-50 mobile-grid-100">
+				<?php query_posts('page_id=4'); /* assign page id SINGLE POST EXCERPT*/ ?><!-- 4 Loeschzug -->
+				<?php if(have_posts) : the_post(); /* start the loop */ ?>
+				<?php the_title( '<h3>' , '</h3>' ); ?>
+				<?php the_excerpt(); /* prints the content */ ?>
+				<?php endif; /* end the loop */ ?>
+			</div>
+			<div class="grid-50 mobile-grid-100">
+				<?php query_posts('page_id=7'); /* assign page id SINGLE POST EXCERPT*/ ?><!-- 7 Jugendfeuerwehr -->
+				<?php if(have_posts) : the_post(); /* start the loop */ ?>
+				<?php the_title( '<h3>' , '</h3>' ); ?>
+				<?php the_excerpt(); /* prints the content */ ?>
+				<?php endif; /* end the loop */ ?>
+			</div>
+			<div class="clear"></div>
+			<div class="grid-33 mobile-grid-100">
+				<?php query_posts('page_id=41'); /* assign page id SINGLE POST EXCERPT*/ ?><!-- 41 First Responder -->
+				<?php if(have_posts) : the_post(); /* start the loop */ ?>
+				<?php the_title( '<h3>' , '</h3>' ); ?>
+				<?php the_excerpt(); /* prints the content */ ?>
+				<?php endif; /* end the loop */ ?>
+			</div>
+			<div class="grid-33 mobile-grid-100">
+				<?php query_posts('page_id=45'); /* assign page id SINGLE POST EXCERPT*/ ?><!-- 45 Spezialeinheit Loeschwasserversorgung -->
+				<?php if(have_posts) : the_post(); /* start the loop */ ?>
+				<?php the_title( '<h3>' , '</h3>' ); ?>
+				<?php the_excerpt(); /* prints the content */ ?>
+				<?php endif; /* end the loop */ ?>
+			</div>
+			<div class="grid-33 mobile-grid-100">
+				<?php query_posts('page_id=48'); /* assign page id SINGLE POST EXCERPT*/ ?><!-- 48 Massenanfall von Verletzten -->
+				<?php if(have_posts) : the_post(); /* start the loop */ ?>
+				<?php the_title( '<h3>' , '</h3>' ); ?>
+				<?php the_excerpt(); /* prints the content */ ?>
+				<?php endif; /* end the loop */ ?>
+			</div>
+			<div class="grid-100">
+				<?php get_footer(); ?>	 
+			</div>
+		</div>
+	</div>
+</body>
+</html>
